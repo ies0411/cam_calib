84961c23af002444e389bd2f4f587637df4b23a4
root <ies0411>
2022-05-13 14:48:52 +0000

    Intrinsic,C++

---
+++ include/calib.h
@@ -0,0 +1,109 @@
+/**
+ * @file calib.h
+ * @author Eunsoo (eslim@superb-ai.com)
+ * @brief mono-cam calibration
+ * @version 0.1
+ * @date 2022-05-13
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef CALIB_CAMERA_HPP
+#define CALIB_CAMERA_HPP
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/calib3d/calib3d_c.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/opencv_modules.hpp>
+#include <vector>
+
+enum CHECK_MOVEMENT {
+    _NOT_ENOUGH_X,
+    _NOT_ENOUGH_Y,
+    _NOT_ENOUGH_Z,
+    _NOT_ENOUGH_ROT,
+    _ENOUGH,
+};
+
+// TODO : roslaunch file and set param
+class setCalibEnv {
+   private:
+    // var
+    ros::NodeHandle nh_;
+    ros::Subscriber image_sub_;
+
+    int checkerboard_rows_num_, checkerboard_colm_num_;
+    std::string image_sub_name_, result_file_intrinsic_, result_file_RT_;
+    double dx_, dy_;  // MEMO . unit : mm
+    int view_cnt_ = 0, view_num_threshold_ = 0;
+    bool finish_ = false, RT_debug_, intrinsic_debug_;
+    std::vector<double> x_movement_DB_, y_movement_DB_, z_movement_DB_, rot_movement_DB_;
+
+    std::vector<std::vector<cv::Point3f> > objpoints_;
+    std::vector<std::vector<cv::Point2f> > imgpoints_;
+    std::vector<cv::Point3f> object_;
+    // function
+    void getParamFunc(ros::NodeHandle &priv_nh);
+    void calibRawimage(const sensor_msgs::Image::ConstPtr &msg);
+    void setObjectPoint();
+    int thresholdBoardMovement(double &&x, double &&y, double &&z, double &&rotation);
+    int checkMovement(std::vector<cv::Point2f> &points);
+
+   public:
+    setCalibEnv(ros::NodeHandle &priv_nh) {
+        getParamFunc(priv_nh);
+        setObjectPoint();
+        image_sub_ = nh_.subscribe(image_sub_name_, 1, &setCalibEnv::calibRawimage, this);
+    }
+    bool getStatus() {
+        return finish_;
+    }
+
+    ~setCalibEnv();
+};
+
+setCalibEnv::~setCalibEnv() {
+    ROS_INFO_STREAM("terminate");
+}
+
+/**
+ * @brief push back checkerboard points(world coordinate)
+ *
+ */
+void setCalibEnv::setObjectPoint() {
+    for (int i = 0; i < checkerboard_rows_num_; i++) {
+        for (int j = 0; j < checkerboard_colm_num_; j++) {
+            object_.push_back(cv::Point3f(j * dx_, i * dy_, 0));
+        }
+    }
+}
+
+/**
+ * @brief Set the Param from launch file
+ *
+ * @param priv_nh : private ros node handler
+ */
+void setCalibEnv::getParamFunc(ros::NodeHandle &priv_nh) {
+    priv_nh.param<int>("checker_x_number", checkerboard_colm_num_, 9);
+    priv_nh.param<int>("checker_y_number", checkerboard_rows_num_, 6);
+    priv_nh.param<double>("dx", dx_, 61.5);
+    priv_nh.param<double>("dy", dy_, 61.5);
+    priv_nh.param<std::string>("image_sub_name", image_sub_name_, "/pylon_camera_node/image_raw");
+    priv_nh.param<int>("view_num", view_num_threshold_, 10);
+
+    priv_nh.param<bool>("RT_debug", RT_debug_, false);
+    priv_nh.param<bool>("Intrinsic_debug", intrinsic_debug_, true);
+    priv_nh.param<std::string>("result_file_intrinsic", result_file_intrinsic_, std::string("/home/catkin_ws/src/mono_cam_calib/result/intrinsic_param.txt"));
+    priv_nh.param<std::string>("result_file_RT", result_file_RT_, std::string("/home/catkin_ws/src/mono_cam_calib/result/RT.txt"));
+}
+
+#endif
---
+++ src/mono_cam_calib.cpp
@@ -0,0 +1,148 @@
+#include "calib.h"
+
+/**
+ * @brief check how much checkerboard was moved
+ *
+ * @param x : movement of x-direction
+ * @param y : movement of y-direction
+ * @param z : movement of z-direction
+ * @param rotation  :movement of rotation
+ * @return Whether there was Enought Movement or not
+ */
+int setCalibEnv::thresholdBoardMovement(double &&x, double &&y, double &&z, double &&rotation) {
+    for (auto &p : x_movement_DB_) {
+        if (std::fabs(p - x) < 5) return CHECK_MOVEMENT::_NOT_ENOUGH_X;
+    }
+    for (auto &p : y_movement_DB_) {
+        if (std::fabs(p - y) < 5) return CHECK_MOVEMENT::_NOT_ENOUGH_Y;
+    }
+    for (auto &p : z_movement_DB_) {
+        if (std::fabs(p - z) < 5) return CHECK_MOVEMENT::_NOT_ENOUGH_Z;
+    }
+    for (auto &p : rot_movement_DB_) {
+        if (std::fabs(p - rotation) < (1 * M_PI / 180.)) return CHECK_MOVEMENT::_NOT_ENOUGH_ROT;
+    }
+    x_movement_DB_.push_back(x);
+    y_movement_DB_.push_back(y);
+    z_movement_DB_.push_back(z);
+    rot_movement_DB_.push_back(rotation);
+    return CHECK_MOVEMENT::_ENOUGH;
+}
+
+int setCalibEnv::checkMovement(std::vector<cv::Point2f> &points) {
+    double max_x = 0, min_x = 10000, min_y = 10000;
+    int max_index, min_index, index = 0;
+    for (auto &p : points) {
+        if (p.x > max_x) {
+            max_x = p.x;
+            max_index = index;
+        }
+        if (p.x < min_x) {
+            min_x = p.x;
+            min_index = index;
+        }
+        if (p.y < min_y) {
+            min_y = p.y;
+        }
+        index++;
+    }
+
+    return thresholdBoardMovement(std::move(min_x), std::move(min_y), std::move(max_x - min_x),
+                                  std::atan2(points[max_index].y - points[min_index].y, points[max_index].x - points[min_index].x));
+}
+
+/**
+ * @brief callback function of Image raw data
+ *
+ * @param msg : images data with ros msgs type
+ */
+void setCalibEnv::calibRawimage(const sensor_msgs::Image::ConstPtr &msg) {
+    // cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();   convert cv2 -> ros topic
+
+    std::vector<cv::Point2f> corner_pts, sqr_corner_pts;
+    cv::Mat frame, image_resized, gray;
+
+    frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+    // MEMO : resolution check,  std::cout << frame.size().width << "." << frame.size().height << std::endl;
+    bool success = cv::findChessboardCorners(gray, cv::Size(checkerboard_colm_num_, checkerboard_rows_num_), corner_pts, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE);
+    cv::drawChessboardCorners(frame, cv::Size(checkerboard_colm_num_, checkerboard_rows_num_), corner_pts, success);
+    if (success) {
+        cv::resize(frame, image_resized, cv::Size(), 0.2, 0.2);
+        cv::imshow("Image", image_resized);
+        cv::waitKey(10);
+        int check_movement = checkMovement(corner_pts);
+        if (CHECK_MOVEMENT::_NOT_ENOUGH_X == check_movement) {
+            ROS_WARN_STREAM("NEED X-MOVEMENT");
+            return;
+        }
+        if (CHECK_MOVEMENT::_NOT_ENOUGH_Y == check_movement) {
+            ROS_WARN_STREAM("NEED Y-MOVEMENT");
+            return;
+        }
+        if (CHECK_MOVEMENT::_NOT_ENOUGH_Z == check_movement) {
+            ROS_WARN_STREAM("NEED Y-MOVEMENT");
+            return;
+        }
+        if (CHECK_MOVEMENT::_NOT_ENOUGH_ROT == check_movement) {
+            ROS_WARN_STREAM("NEED ROT-MOVEMENT");
+            return;
+        }
+
+        if (view_cnt_++ < view_num_threshold_) {
+            ROS_INFO("DETECT %d", view_cnt_);
+            cv::TermCriteria criteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.001);
+            cv::cornerSubPix(gray, corner_pts, cv::Size(11, 11), cv::Size(-1, -1), criteria);
+            objpoints_.push_back(object_);
+            imgpoints_.push_back(corner_pts);
+            return;
+        }
+        finish_ = true;
+
+    } else {
+        ROS_WARN_STREAM("CAN NOT FIND BOARD CORNER");
+        return;
+    }
+
+    cv::Mat cameraMatrix, distCoeffs, R, T;
+    cv::calibrateCamera(objpoints_, imgpoints_, cv::Size(gray.cols, gray.rows), cameraMatrix, distCoeffs, R, T);
+    if (RT_debug_) {
+        std::cout << "R : "
+                  << R << std::endl;
+        std::cout << "T : "
+                  << R << std::endl;
+    }
+    if (intrinsic_debug_) {
+        std::cout << "cameraMatrix : \n"
+                  << cameraMatrix << std::endl;
+        std::cout << "distCoeffs : \n"
+                  << distCoeffs << std::endl;
+    }
+
+    // TODO txt -> YAML or Json
+
+    std::ofstream results;
+    results.open(result_file_intrinsic_, std::ofstream::out | std::ofstream::trunc);
+    results << cameraMatrix << "\n"
+            << distCoeffs;
+    results.close();
+    results.open(result_file_RT_, std::ofstream::out | std::ofstream::trunc);
+    results << R << "\n======\n"
+            << T;
+    results.close();
+}
+
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "calib_node");
+    ros::NodeHandle nh("~");
+    setCalibEnv calib(nh);
+
+    ros::Rate rate(100);
+    while (ros::ok()) {
+        // return when finishing calibration
+        if (calib.getStatus()) break;
+        ros::spinOnce();
+    }
+
+    return 0;
+}
